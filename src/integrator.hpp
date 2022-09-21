065589e0e718b7c2fae51d6bf3dad52a1198906b
MaximeCailles <max.cailles74210@gmail.com>
2022-09-21 11:04:51 -0400

    Base

---
+++ src/integrator.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include "math.h"
+#include "particle.h"
+
+class integrator
+{
+    public:
+
+    void update(particle &p, float t){
+        
+    }
+
+    void updatePosition(particle &p, float t){
+
+    }
+
+    void updateVelocity(particle &p, float t){
+
+    }
+
+    
+
+
+}
